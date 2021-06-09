aa63fcdcf1d6d789a6a24015afe7691af8a68e74
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-06-09 20:29:25 +0530

    Added Custom ScrollBar

--- shared/theme.js
+++ shared/theme.js
@@ -1,16 +1,38 @@
 import { createMuiTheme } from '@material-ui/core';
 
 export const theme = createMuiTheme({
+	overrides: {
+		MuiCssBaseline: {
+			'@global': {
+				'*': {
+					'scrollbar-width': 'thin',
+				},
+				'*::-webkit-scrollbar': {
+					width: '15px',
+					height: '15px',
+				},
+				'*::-webkit-scrollbar-track': {
+					background: '#f4f4f4',
+				},
+				'*::-webkit-scrollbar-thumb': {
+					background: '#16a085',
+				},
+				'*::-webkit-scrollbar-thumb:hover': {
+					background: '#1abc9c90',
+				},
+			},
+		},
+	},
 	breakpoints: {
 		values: {
-		  xs: 300,
-		  sm: 640,
-		  md: 768,
-		  lg: 1024,
-		  xl: 1280,
-		  '2xl': 1536
+			xs: 300,
+			sm: 640,
+			md: 768,
+			lg: 1024,
+			xl: 1280,
+			'2xl': 1536,
 		},
-	  },
+	},
 	palette: {
 		primary: {
 			main: '#1abc9c',
