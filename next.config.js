c8641b33ec5578f3a2f6e3d11a70cc30746471e5
Vishwajeet Raj <ext-vishwajeet.raj@graphy.com>
2023-09-03 00:01:31 +0530

    added changes to next11

--- next.config.js
+++ next.config.js
@@ -12,4 +12,9 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
